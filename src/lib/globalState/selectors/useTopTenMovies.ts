a46af3da25ff4d99bb41b94df60365f6b592d35a
zeno_begnini <zeno.begnini@gmail.com>
2023-07-13 10:51:55 +0200

    fix task 3

--- src/lib/globalState/selectors/useTopTenMovies.ts
+++ src/lib/globalState/selectors/useTopTenMovies.ts
@@ -1,11 +1,15 @@
 import { useAllMovies } from "./useAllMovies";
-
 /**
  * Return the 10 most voted movies
  */
 export const useTopTenMovies = () => {
   const movies = useAllMovies();
-  // TODO: reorder only best 10 movies by vote, desc (movie with more votes first)
-  movies.sort((a, b) => a.votes - b.votes);
-  return movies.reverse;
+
+  // Reorder movies by vote in descending order
+  movies.sort((a, b) => b.votes - a.votes);
+
+  // Get top 10 movie titles
+  const topTen: string[] = movies.slice(0, 10).map(movie => movie.title);
+
+  return topTen;
 };
