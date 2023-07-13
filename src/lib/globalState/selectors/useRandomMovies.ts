082fef0d5a4047e7f9c20fbcba041d30cad68a2c
zeno_begnini <zeno.begnini@gmail.com>
2023-07-13 10:31:29 +0200

    Task 2

--- src/lib/globalState/selectors/useRandomMovies.ts
+++ src/lib/globalState/selectors/useRandomMovies.ts
@@ -1,12 +1,15 @@
 import { useGlobalState } from "../GlobalStateContext";
-
+import { useMemo } from "react";
 /**
  * Return 2 random movies
  */
 export const useRandomMovies = () => {
   const { state } = useGlobalState();
 
-  // TODO: return only 2 random movies (use useMemo)
+  const randomMovies = useMemo(() => {
+    const shuffledMovies = state.movies.sort(() => 0.5 - Math.random());
+    return shuffledMovies.slice(0, 2);
+  }, [state.movies]);
 
-  return state.movies;
+  return randomMovies;
 };
